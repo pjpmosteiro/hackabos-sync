8a67cdcf602d364fd9967b5cbabd455555576a68
Pablo Prieto <pjpmosteiro@gmail.com>
2019-03-09 10:24:51 +0100

    User router ajustado para search

--- modulo_4/social-network2/webserver/routes/user-router.js
+++ modulo_4/social-network2/webserver/routes/user-router.js
@@ -7,7 +7,7 @@ const getUserProfile = require('../controllers/user/get-user-profile');
 const checkJwtToken = require('../controllers/session/check-jwt-token');
 const updateUserProfile = require('../controllers/user/update-user-profile');
 const uploadAvatar = require('../controllers/user/upload-avatar');
-// const userSearch = require('../controllers/user/search-user');
+const searchUsers = require('../controllers/user/search-user');
 
 const upload = multer();
 const router = express.Router();
@@ -15,8 +15,7 @@ const router = express.Router();
 router.get('/user', checkJwtToken, getUserProfile);
 router.put('/user', checkJwtToken, updateUserProfile);
 router.post('/user/avatar', checkJwtToken, upload.single('avatar'), uploadAvatar);
-router.get('/user/search', checkJwtToken, (req, res, next) => {
-  res.send([]);
-});
 
-module.exports = router
+router.get('/user/search', checkJwtToken, searchUsers);
+
+module.exports = router;
