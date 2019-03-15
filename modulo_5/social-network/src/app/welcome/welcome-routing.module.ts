fb70c0f7b1168980de51eaf7be68b1fd96477b3f
Pablo Prieto <pjpmosteiro@gmail.com>
2019-03-15 21:29:40 +0100

    Welcome componet

--- modulo_5/social-network/src/app/welcome/welcome-routing.module.ts
+++ modulo_5/social-network/src/app/welcome/welcome-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { WelcomeComponent } from './components/welcome/welcome.welcome.component';
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent }
 ];
