a80d48198b6cc38a201c492a93a98903038e3138
Pablo Prieto <pjpmosteiro@gmail.com>
2019-03-16 11:20:13 +0100

    Generado site layout

--- modulo_5/social-network/src/app/shared/shared.module.ts
+++ modulo_5/social-network/src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SiteLayoutComponent } from './components/site-layout/site-layout.component';
+import { SiteLayoutHeaderComponent } from './components/site-layout-header/site-layout-header.component';
+import { SiteLayoutFooterComponent } from './components/site-layout-footer/site-layout-footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SiteLayoutComponent, SiteLayoutHeaderComponent, SiteLayoutFooterComponent],
   imports: [
     CommonModule
   ]
