5b51b8d427d24bda6fa2bd1ee2f8c838ea161b92
Pablo Prieto <pjpmosteiro@gmail.com>
2019-03-16 13:19:07 +0100

    Añadido icono

--- modulo_5/social-network/src/app/shared/components/site-layout-header/site-layout-header.component.ts
+++ modulo_5/social-network/src/app/shared/components/site-layout-header/site-layout-header.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { faHackerrank } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'sn-site-layout-header',
   templateUrl: './site-layout-header.component.html',
   styleUrls: ['./site-layout-header.component.scss']
 })
-export class SiteLayoutHeaderComponent implements OnInit {
+export class SiteLayoutHeaderComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  logo = faHackerrank;
+  title = 'The Social Network';
 }
