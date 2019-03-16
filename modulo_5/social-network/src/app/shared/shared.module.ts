918d169cd0e96bbf3c3de157714863853b0267ab
Pablo Prieto <pjpmosteiro@gmail.com>
2019-03-16 11:53:17 +0100

    Añadido site-layout a pag principal

--- modulo_5/social-network/src/app/shared/shared.module.ts
+++ modulo_5/social-network/src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { SiteLayoutComponent } from './components/site-layout/site-layout.component';
 import { SiteLayoutHeaderComponent } from './components/site-layout-header/site-layout-header.component';
 import { SiteLayoutFooterComponent } from './components/site-layout-footer/site-layout-footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SiteLayoutComponent, SiteLayoutHeaderComponent, SiteLayoutFooterComponent],
   imports: [
-    CommonModule
+    CommonModule, SiteLayoutComponent, SiteLayoutHeaderComponent, SiteLayoutFooterComponent
   ]
 })
 export class SharedModule { }
--- modulo_5/social-network/src/app/shared/welcome/welcome.module.ts
+++ modulo_5/social-network/src/app/shared/welcome/welcome.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [WelcomeComponent],
   imports: [CommonModule, WelcomeRoutingModule]
 })
-export class WelcomeModule {}
+export class WelcomeModule { }
