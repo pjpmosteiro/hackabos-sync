71f95a49fb7ad4277a971ca8d5dd3586eabcf863
Pablo Prieto <pjpmosteiro@gmail.com>
2019-03-23 10:04:07 +0100

    Copy desde Yago

--- modulo_5/social-network/src/app/dashboard/dashboard.module.ts
+++ modulo_5/social-network/src/app/dashboard/dashboard.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DashboardComponent } from './containers/dashboard/dashboard.component';
+import { DashboardNavComponent } from './components/dashboard-nav/dashboard-nav.component';
+import { DashboardHeaderComponent } from './components/dashboard-header/dashboard-header.component';
+import { DashboardAsideComponent } from './components/dashboard-aside/dashboard-aside.component';
+import { DashboardFooterComponent } from './components/dashboard-footer/dashboard-footer.component';
+import { WallComponent } from './containers/wall/wall.component';
+import { FriendsComponent } from './containers/friends/friends.component';
+import { MyAccountComponent } from './containers/my-account/my-account.component';
+import { MyaccountComponent } from './myaccount/myaccount.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule
-  ]
+  declarations: [
+    DashboardComponent,
+    DashboardNavComponent,
+    DashboardHeaderComponent,
+    DashboardAsideComponent,
+    DashboardFooterComponent,
+    WallComponent,
+    FriendsComponent,
+    MyAccountComponent,
+    MyaccountComponent
+  ],
+  imports: [CommonModule, DashboardRoutingModule]
 })
-export class DashboardModule { }
+export class DashboardModule {}
---
+++ modulo_5/social-network/src/app/dashboard/myaccount/myaccount.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sn-myaccount',
+  templateUrl: './myaccount.component.html',
+  styleUrls: ['./myaccount.component.scss']
+})
+export class MyaccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
