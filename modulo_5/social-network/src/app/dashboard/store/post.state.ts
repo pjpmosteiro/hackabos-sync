81e65b13cfb4c68e1b6f47db6911d4a2848504a4
pablo (portatil HP) <publico@pablojprieto.com.es>
2019-03-23 18:13:34 +0100

    Ajuste del modulo de wall

--- modulo_5/social-network/src/app/dashboard/store/post.state.ts
+++ modulo_5/social-network/src/app/dashboard/store/post.state.ts
@@ -1,38 +1,48 @@
-import { State, Store, Action } from '@ngxs/store';
+import { State, Store, Action, StateContext, Selector } from '@ngxs/store';
 import { PostCollection } from '../dashboard.models';
 import { PostService } from '../services/post.service';
-import { GetPostsSuccess, GetPostsFailed, GetPosts } from './post.action';
+import { GetPosts, GetPostsSuccess, GetPostsFailed } from './post.action';
 import { tap, catchError } from 'rxjs/operators';
-import { dispatch } from 'rxjs/internal/observable/range';
-
 
 @State<PostCollection>({
     name: 'posts',
     defaults: {}
 })
 export class PostState {
+    @Selector()
+    static getPost(state: PostCollection) {
+        return Object.values(state);
+    }
 
     constructor(private store: Store, private postService: PostService) { }
 
     @Action(GetPosts)
     getPosts({ dispatch }: StateContext<PostCollection>) {
         return this.postService.getWall().pipe(
-            tap(post => dispatch(new GetPostsSuccess(posts))),
+            tap(posts => dispatch(new GetPostsSuccess(posts))),
             catchError(error => dispatch(new GetPostsFailed(error.error)))
         );
     }
 
     @Action(GetPostsSuccess)
-    GetPostsSuccess(
+    getPostsSuccess(
         { setState }: StateContext<PostCollection>,
         { posts }: GetPostsSuccess
     ) {
+        const orderedPosts = posts.sort((p1, p2) => {
+            return p2.createdAt - p1.createdAt;
+        });
 
-        setState(posts);
+        setState(
+            orderedPosts.reduce((draft, post) => {
+                draft[post.id] = post;
+                return draft;
+            }, {})
+        );
     }
 
-    @Action(GetPostsFailed)
-    GetPostsFailed(ctx: StateContext<PostCollection>, action: GetPostsFailed) {
-        console.log(errors)
+    @Action([GetPostsFailed])
+    errors(ctx: StateContext<PostCollection>, { errors }: GetPostsFailed) {
+        console.log(errors);
     }
 }
