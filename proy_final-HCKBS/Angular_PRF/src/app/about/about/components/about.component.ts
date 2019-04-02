31d56e916b41956e837d4d7889c42013702cda7f
Pablo (Asus) <pjpmosteiro@gmail.com>
2019-04-02 07:07:27 +0200

    Inicio api

--- proy_final-HCKBS/Angular_PRF/src/app/about/about/components/about.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sn-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.html']
-})
-
-export class AboutComponent { };
