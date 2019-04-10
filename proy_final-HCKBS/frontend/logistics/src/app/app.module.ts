bb9268e553e445530c5fa1031c906795bbdeafe9
pablo (portatil HP) <publico@pablojprieto.com.es>
2019-04-10 23:16:08 +0200

    Añadido scnet para base de logistics front

--- proy_final-HCKBS/frontend/logistics/src/app/app.module.ts
+++ proy_final-HCKBS/frontend/logistics/src/app/app.module.ts
@@ -12,13 +12,13 @@ import { HomeComponent } from "./home/home.component";
 import { RoutingModule } from "./routing/routing.module";
 import { HeaderComponent } from "./navigation/header/header.component";
 import { SidenavListComponent } from "./navigation/sidenav-list/sidenav-list.component";
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './auth/containers/login/login.component';
+import { RegisterComponent } from './forms/register/register.component';
 import { Section1Component } from './home/components/section1/section1.component';
 import { Section2Component } from './home/components/section2/section2.component';
 import { Section3Component } from './home/components/section3/section3.component';
 import { SectionBarComponent } from './home/components/section-bar/section-bar.component';
-import { UserComponent } from './user/user.component';
+import { UserComponent } from './pages/user/user.component';
 import { CustomMaterialModule } from "./layout/material/material.module";
 import { ContentComponent } from './home/components/content/content.component';
 
--- proy_final-HCKBS/frontend/logistics/src/app/routing/routing.module.ts
+++ proy_final-HCKBS/frontend/logistics/src/app/routing/routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
-import { LoginComponent } from '../login/login.component';
-import { RegisterComponent } from '../register/register.component';
-import { UserComponent } from '../user/user.component';
+import { LoginComponent } from '../forms/login/login.component';
+import { RegisterComponent } from '../forms/register/register.component';
+import { UserComponent } from '../pages/user/user.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
