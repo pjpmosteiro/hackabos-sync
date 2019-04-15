9463b6dec7b174dba7bf4ccdf455c3dfc20b5637
Pablo vscode HP <pjpmosteiro@gmail.com>
2019-04-15 14:44:28 +0200

    [BACKEND] Reajuste de server.js

--- proy_final-HCKBS/backend/logistic_api/server/server.js
+++ proy_final-HCKBS/backend/logistic_api/server/server.js
@@ -1,56 +1,23 @@
+
 'use strict';
 
-let loopback = require('loopback');
-let boot = require('loopback-boot');
+var loopback = require('loopback');
+var boot = require('loopback-boot');
 
-let app = (module.exports = loopback());
+var app = module.exports = loopback();
 
 app.start = function () {
   // start the web server
   return app.listen(function () {
     app.emit('started');
-    let baseUrl = app.get('url').replace(/\/$/, '');
+    var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
-      let explorerPath = app.get('loopback-component-explorer').mountPath;
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
 };
-
-app.use(
-  loopback.token({
-    model: app.models.accessToken,
-    currentUserLiteral: 'me',
-    bearerTokenBase64Encoded: false, // here
-  })
-);
-
-//TODO: insertar middleware loopback-jwt
-
-const loopbackJWT = require("loopback-jwt-advanced");
-const auth = loopbackJWT(app, {
-  verify: function (req) {
-    let jwt = req.user;
-    if ("some custom verification fails") { throw new Error("Token invalid."); }
-  },
-  beforeCreate: function (userObj, req) {
-    let jwt = req.user;
-    // add custom fields to the user object within the database
-    userObj.emailVerified = jwt.email_verified;
-    userObj.username = jwt.nickname;
-    userObj.remoteId = jwt.sub;
-  }
-});
-
-app.use("/<path>", auth.authenticated);
-
-app.use(function (err, req, res, next) {
-  // beautify error for loopback.errorHandler()
-  if (err.name === "UnauthorizedError") { err = { status: 401, message: " Missing or invalid token" }; }
-  next(err);
-});
-
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
@@ -58,6 +25,6 @@ boot(app, __dirname, function (err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module) app.start();
+  if (require.main === module)
+    app.start();
 });
-
