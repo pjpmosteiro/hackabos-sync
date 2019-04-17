9bb359c353fa86e545818f549238004e0b3d421d
Pablo vscode HP <pjpmosteiro@gmail.com>
2019-04-17 17:46:32 +0200

    ajustado tracking para auth con get

--- proy_final-HCKBS/frontend/logistics/src/app/pages/tracking/tracking.component.ts
+++ proy_final-HCKBS/frontend/logistics/src/app/pages/tracking/tracking.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-tracking',
@@ -8,19 +11,13 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./tracking.component.scss']
 })
 
-export class TrackingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class trackingComponent {
+  constructor(public http: HttpClient) { }
+  public ping() {
+    this.http.get(`${environment.apiBaseUrl}/lg01s`)
+      .subscribe(
+        data => console.log(data),
+        err => console.log(err)
+      );
   }
-  const appRoutes: Routes = [
-    {
-      path: 'tracking/:id',
-      component: TrackingComponent,
-      data: { title: 'Detalle del envio' }
-    },
-  ]
-
-
 }
