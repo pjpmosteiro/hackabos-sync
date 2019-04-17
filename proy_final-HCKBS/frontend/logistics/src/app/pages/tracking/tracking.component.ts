8b6e261bffc9de41b28a4d562081bf154f767b09
Pablo (Newton) <publico@pablojprieto.com.es)>
2019-04-18 01:49:44 +0200

    creado box tracking

--- proy_final-HCKBS/frontend/logistics/src/app/pages/tracking/tracking.component.ts
+++ proy_final-HCKBS/frontend/logistics/src/app/pages/tracking/tracking.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-
 import { environment } from 'src/environments/environment';
+import { Store, Actions, ofAction } from '@ngxs/store';
 
 @Component({
   selector: 'app-tracking',
@@ -12,12 +12,22 @@ import { environment } from 'src/environments/environment';
 })
 
 export class TrackingComponent {
-  constructor(public http: HttpClient) { }
-  public ping() {
+  constructor(public http: HttpClient,
+    private fb: FormBuilder,
+    private store: Store,
+    private actions$: Actions) { }
+  //Envío de datos al backend
+  public send() {
     this.http.get(`${environment.apiBaseUrl}/lg01s`)
       .subscribe(
         data => console.log(data),
         err => console.log(err)
       );
   }
+
+  registerForm = this.fb.group(
+    {
+      track_number: ['']
+
+    })
 }
