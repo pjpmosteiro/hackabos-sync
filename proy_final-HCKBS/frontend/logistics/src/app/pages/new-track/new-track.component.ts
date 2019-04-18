f4c580438d14fcf69d75804517181f765b75c8ba
Pablo (Newton) <publico@pablojprieto.com.es)>
2019-04-18 04:23:45 +0200

    Generado New-Track Component

---
+++ proy_final-HCKBS/frontend/logistics/src/app/pages/new-track/new-track.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-track',
+  templateUrl: './new-track.component.html',
+  styleUrls: ['./new-track.component.scss']
+})
+export class NewTrackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
