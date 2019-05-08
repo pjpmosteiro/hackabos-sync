021310d49d6bc84a99903dc1b96b67f10dea9e98
Pablo (Newton) <publico@pablojprieto.com.es)>
2019-05-09 01:29:03 +0200

    Ajustado modelo de mongo para track

---
+++ proy_final-HCKBS/backend/logistic_api/models/log-model.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const {
+  Schema
+} = mongoose;
+
+const postSchema = new Schema({
+  date: Date,
+  remit: String,
+  dest: String,
+  user: Number,
+  status: {
+    type: String,
+    default: 'Registrado'
+  },
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Log;
+
