9cfd66ff1b43dbe88db1af91f35dad8ae12e18d5
HP Arcadia <pablo@pablojprieto.com.es>
2019-05-08 13:45:34 +0200

    Ajuste cierre backend -- apertura de nuevo branch

--- proy_final-HCKBS/backend/logistic_api/server/boot/routes.js
+++ proy_final-HCKBS/backend/logistic_api/server/boot/routes.js
@@ -2,8 +2,11 @@
 
 module.exports = function (app) {
     let router = app.loopback.Router();
-    router.get('/ping', function (req, res) {
-        res.send('Serividor API REST OK, sistema listo! :)');
+    router.get('/api/lg01s', function (req, res) {
+        res.send('SERVIDOR API LOGISTICS --  FUERA DE SERVICIO // LOGISTICS API SERVER -- OUT OF SERVICE');
+    });
+    router.post('/api/Users', function (req, res) {
+        res.send('SERVIDOR API LOGISTICS --  FUERA DE SERVICIO // LOGISTICS API SERVER -- OUT OF SERVICE');
     });
     app.use(router);
 };
