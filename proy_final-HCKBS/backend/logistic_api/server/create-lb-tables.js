499657b4dec3fe5d22c770492fb5ab1847b1da48
HP Arcadia <publico@pablojprieto.com.es>
2019-05-08 13:58:34 +0200

    ajuste

--- proy_final-HCKBS/backend/logistic_api/server/create-lb-tables.js
+++
@@ -1,9 +0,0 @@
-
-let server = require('./server');
-let ds = server.dataSources.db;
-let lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
-ds.automigrate(lbTables, function (er) {
-  if (er) throw er;
-  console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
-  ds.disconnect();
-});
