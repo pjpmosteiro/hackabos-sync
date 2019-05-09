795a9ded34cca21a6ce7b0b11bb15a7d36e9fa34
Pablo (Newton) <publico@pablojprieto.com.es)>
2019-05-09 02:36:13 +0200

    comentado enrutado

--- proy_final-HCKBS/backend/logistic_api/app/api/routes/users.js
+++ proy_final-HCKBS/backend/logistic_api/app/api/routes/users.js
@@ -1,6 +1,9 @@
+// Router for users
 const express = require('express');
 const router = express.Router();
 const userController = require('../app/api/controllers/users');
+
 router.post('/register', userController.create);
 router.post('/authenticate', userController.authenticate);
+
 module.exports = router;
